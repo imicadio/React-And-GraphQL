7f6daf164acdbe191032f09de8d7248369d5b939
Michal Jeszko <michal.jeszko@sprint.pl>
2021-02-23 22:05:03 +0100

    An Intro to Styled Components and CSS

--- frontend/.next/server/pages/_document.js
+++ frontend/.next/server/pages/_document.js
@@ -826,7 +826,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/document */ "./node_modules/next/document.js");
 /* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_document__WEBPACK_IMPORTED_MODULE_1__);
 
-var _jsxFileName = "C:\\Users\\Micha\u0142\\Advanced-React\\sick-fits\\frontend\\pages\\_document.js";
+var _jsxFileName = "C:\\Users\\Micha\u0142\\shop-react\\React-And-GraphQL\\frontend\\pages\\_document.js";
 
 class MyDocument extends next_document__WEBPACK_IMPORTED_MODULE_1___default.a {
   render() {
--- frontend/.next/server/pages/orders.js
+++ frontend/.next/server/pages/orders.js
@@ -106,7 +106,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ "react/jsx-dev-runtime");
 /* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);
 
-var _jsxFileName = "C:\\Users\\Micha\u0142\\Advanced-React\\sick-fits\\frontend\\pages\\orders.js";
+var _jsxFileName = "C:\\Users\\Micha\u0142\\shop-react\\React-And-GraphQL\\frontend\\pages\\orders.js";
 function OrderPage() {
   return /*#__PURE__*/Object(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__["jsxDEV"])("div", {
     children: /*#__PURE__*/Object(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__["jsxDEV"])("p", {
