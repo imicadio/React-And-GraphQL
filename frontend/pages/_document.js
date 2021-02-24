c04acd377607b60a10128d3a01d1acc8699e0ffb
Michal Jeszko <michal.jeszko@sprint.pl>
2021-02-24 15:03:29 +0100

    Visualizing Route Changes - progress

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="pl-PL">
-                {/* <head></head> */}
+                <Head />
                 <body>
                     <Main/>
                     <NextScript/>
