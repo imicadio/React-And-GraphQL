8baf052206560a92333807d53edad836af7bf9e3
Michal Jeszko <michal.jeszko@sprint.pl>
2021-04-03 14:25:03 +0200

    Displaying Single Items, Routing and SEO

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,6 @@
+import Page from '../components/Page';
 import NProgress from 'nprogress';
 import Router from 'next/router';
-import Page from "../components/Page";
 import '../components/styles/nprogress.css';
 import { ApolloProvider } from '@apollo/client';
 import withData from '../lib/withData';
@@ -9,23 +9,23 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-function MyApp({ Component, pageProps, apollo}) {
-    return (
-        <ApolloProvider client={apollo}>
-            <Page>
-                <Component {...pageProps} />
-            </Page>
-        </ApolloProvider>
-    );
-}
+const MyApp = ({ Component, pageProps, apollo }) => {
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
 
-MyApp.getInitialProps = async function({ Component, ctx }) {
-    let pagePros = {};
-    if(Component.getInitialProps) {
-        pagePros = await Component.getInitialProps(ctx);
-    }
-    pagePros.query = ctx.query;
-    return { pagePros };
-}
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitalProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
 
 export default withData(MyApp);
