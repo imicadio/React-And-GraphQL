0b69f3394dd6c796c34d43b6df429b4146114612
Michal Jeszko <michal.jeszko@sprint.pl>
2021-04-06 17:36:14 +0200

    Pagination Links

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,7 +1,12 @@
+import Pagination from "../components/Pagination";
 import Products from "../components/Products";
 
 export default function OrderPage() {
-    return <div>
-        <Products />
-    </div>
+    return (
+        <div>
+            <Pagination page={1} />
+            <Products />
+            <Pagination page={1} />
+        </div>
+    );
 }
