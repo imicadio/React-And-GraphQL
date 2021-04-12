f4561d0d3acf72b402aed44e35faf5d69e1e0485
Michal Jeszko <michal.jeszko@sprint.pl>
2021-04-12 17:05:30 +0200

    Password Reset - sending the email

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import { ProductImage } from './schemas/ProductImage';
 import { User } from './schemas/User';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -23,7 +24,8 @@ const { withAuth } = createAuth({
     },
     passwordResetLink: {
         async sendToken(args) {
-            console.log(args);
+            // send the email
+            await sendPasswordResetEmail(args.token, args.identity);
         }
     }
 });
